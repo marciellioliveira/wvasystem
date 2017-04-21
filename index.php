5d0a1c886a49b2555907a79a2079a4cf4c4655a2
Marcielli Oliveira <marciellileticiaol@gmail.com>
2017-04-21 19:35:14 -0300

    Front-End finalizado

---
+++ index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Sistema de Postagem com PHP PDO</title>
+		<link rel="stylesheet" type="text/css" href="css/reset.css" media="all">
+		<link rel="stylesheet" type="text/css" href="css/estilo.css" media="all">
+	</head>
+
+	<body>
+		
+		<div class="divcenter">
+			
+			<ul class="boxposts">
+				<li>
+					<span class="thumb">
+						<img src="imagens/thumb.jpg" alt="" title="" width="166" height="166">
+					</span>
+					<span class="content">
+						<h1>Título da Notícia</h1>
+						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+						<div class="footer_post">
+							<a href="#">Leia o artigo completo</a>
+							<span class="data_post">Data de Publicação: <strong>00/00/0000</strong></span>
+						</div> <!-- footer_post -->
+					</span>
+				</li>
+
+				<li>
+					<span class="thumb">
+						<img src="imagens/thumb.jpg" alt="" title="" width="166" height="166">
+					</span>
+					<span class="content">
+						<h1>Título da Notícia</h1>
+						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+						<div class="footer_post">
+							<a href="#">Leia o artigo completo</a>
+							<span class="data_post">Data de Publicação: <strong>00/00/0000</strong></span>
+						</div> <!-- footer_post -->
+					</span>
+				</li>
+
+				<li>
+					<span class="thumb">
+						<img src="imagens/thumb.jpg" alt="" title="" width="166" height="166">
+					</span>
+					<span class="content">
+						<h1>Título da Notícia</h1>
+						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+						<div class="footer_post">
+							<a href="#">Leia o artigo completo</a>
+							<span class="data_post">Data de Publicação: <strong>00/00/0000</strong></span>
+						</div> <!-- footer_post -->
+					</span>
+				</li>
+
+				<li>
+					<span class="thumb">
+						<img src="imagens/thumb.jpg" alt="" title="" width="166" height="166">
+					</span>
+					<span class="content">
+						<h1>Título da Notícia</h1>
+						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+						<div class="footer_post">
+							<a href="#">Leia o artigo completo</a>
+							<span class="data_post">Data de Publicação: <strong>00/00/0000</strong></span>
+						</div> <!-- footer_post -->
+					</span>
+				</li>
+
+				<li>
+					<span class="thumb">
+						<img src="imagens/thumb.jpg" alt="" title="" width="166" height="166">
+					</span>
+					<span class="content">
+						<h1>Título da Notícia</h1>
+						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+						<div class="footer_post">
+							<a href="#">Leia o artigo completo</a>
+							<span class="data_post">Data de Publicação: <strong>00/00/0000</strong></span>
+						</div> <!-- footer_post -->
+					</span>
+				</li>
+				
+			</ul>
+
+		</div> <!-- divcenter -->
+
+	</body>
+
+</html>
+
+<!-- 
+Seguindo video aula: https://www.youtube.com/watch?v=GmLkhBLqZ7s&list=PLv_jn1il3u24GnGpIh2YO2QYC3BpvdXE3&index=2#t=247.009278
+-->
